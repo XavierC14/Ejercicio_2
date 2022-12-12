02c9f298aabcb2a1d35960ba8a03a1341525432d
XavierC14 <xavicm1407@gmail.com>
2022-12-12 18:41:41 +0100

    a

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,6 +23,7 @@ class UserFactory extends Factory
             // 'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'edad' => rand(18,30),
+            //Campo roles
             'rol' => fake()->randomDigit(1, 4),
             'remember_token' => Str::random(10)
         ];
